6239c3d5372274c86c8159bd089f3432df317fa1
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2024-12-26 15:59:03 +0530

    added app:detection

--- YdhyaAI/settings.py
+++ YdhyaAI/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "arogya_ai.apps.ArogyaAiConfig"
+    "arogya_ai.apps.ArogyaAiConfig",
+    "detection.apps.DetectionConfig"
 ]
 
 MIDDLEWARE = [
--- YdhyaAI/urls.py
+++ YdhyaAI/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('arogya_ai.urls')),
+    path('detection/', include('detection.urls'))
 ]
